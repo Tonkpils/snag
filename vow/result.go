a30bf4077a99829e522b1e0f8c3d20d76f7908e2
Steven Berlanga <zabawaba99@gmail.com>
2015-07-18 20:19:53 -0400

    removing result.String() function until it's actually needed

--- vow/result.go
+++ vow/result.go
@@ -1,9 +1,6 @@
 package vow
 
-import (
-	"fmt"
-	"os"
-)
+import "os"
 
 // Result represents the outcome of the Vow that was run
 type Result struct {
@@ -21,31 +18,4 @@ type cmdResult struct {
 	ps     *os.ProcessState
 	output []byte
 	failed bool
-}
-
-func (r *Result) String() string {
-	s := "To Result:\n"
-	s += fmt.Sprintf("\tRan %d of %d commands\n", r.executed, len(r.results))
-
-	for i := 0; i < len(r.results); i++ {
-		cmdrslt := r.results[i]
-
-		var outcome string
-		switch {
-		case i < r.executed-1 ||
-			(i == r.executed-1 && !cmdrslt.failed):
-			outcome = "SUCCESS - "
-		case i >= r.executed:
-			outcome = "SKIPPED - "
-		case i == r.executed-1:
-			outcome = "FAILED  - "
-		}
-
-		s += fmt.Sprintf("\t%s%s - %v\n", outcome, cmdrslt.command, cmdrslt.args)
-
-		if cmdrslt.failed {
-			s += fmt.Sprintf("Output:\n%s\n", cmdrslt.output)
-		}
-	}
-	return s
 }
