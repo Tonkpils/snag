450fbcfbea760b7a3757a93ea598f2d43d8de4cb
Steven Berlanga <zabawaba99@gmail.com>
2015-08-01 03:36:22 -0400

    ensuring that commands don't print after they've been killed

--- vow/promise.go
+++ vow/promise.go
@@ -5,47 +5,53 @@ import (
 	"io"
 	"os/exec"
 	"strings"
+	"sync/atomic"
 	"syscall"
 	"time"
 )
 
+type bufCloser struct {
+	bytes.Buffer
+}
+
+func (bc *bufCloser) Close() error {
+	bc.Reset()
+	return nil
+}
+
 type promise struct {
-	cmd  *exec.Cmd
-	stop chan struct{}
+	cmd    *exec.Cmd
+	killed *int32
 }
 
 func newPromise(name string, args ...string) *promise {
 	return &promise{
-		cmd:  exec.Command(name, args...),
-		stop: make(chan struct{}),
+		cmd:    exec.Command(name, args...),
+		killed: new(int32),
 	}
 }
 
 func (p *promise) Run(w io.Writer) (result cmdResult, err error) {
-	defer close(p.stop)
-
-	var b bytes.Buffer
-
-	p.cmd.Stdout = &b
-	p.cmd.Stderr = &b
+	buf := new(bufCloser)
+	p.cmd.Stdout = buf
+	p.cmd.Stderr = buf
 
 	// TODO: make the printing prettier
 	w.Write([]byte("snag: " + strings.Join(p.cmd.Args, " ") + " - In Progress"))
 	if err := p.cmd.Start(); err != nil {
-		w.Write([]byte("\b\b\b\b\b\b\b\b\b\b\bFailed       \n"))
-		w.Write([]byte(err.Error() + "\n"))
+		p.writeIfAlive(w, []byte("\b\b\b\b\b\b\b\b\b\b\bFailed       \n"))
+		p.writeIfAlive(w, []byte(err.Error()+"\n"))
 		return result, err
 	}
 
 	if err := p.cmd.Wait(); err != nil {
 		result.failed = true
-		w.Write([]byte("\b\b\b\b\b\b\b\b\b\b\bFailed       \n"))
+		p.writeIfAlive(w, []byte("\b\b\b\b\b\b\b\b\b\b\bFailed       \n"))
 	} else {
-
-		w.Write([]byte("\b\b\b\b\b\b\b\b\b\b\bPassed       \n"))
+		p.writeIfAlive(w, []byte("\b\b\b\b\b\b\b\b\b\b\bPassed       \n"))
 	}
 
-	w.Write(b.Bytes())
+	p.writeIfAlive(w, buf.Bytes())
 
 	result.ps = p.cmd.ProcessState
 	result.command = p.cmd.Path
@@ -53,18 +59,15 @@ func (p *promise) Run(w io.Writer) (result cmdResult, err error) {
 	return
 }
 
+func (p *promise) writeIfAlive(w io.Writer, b []byte) {
+	if atomic.LoadInt32(p.killed) == 0 {
+		w.Write([]byte(b))
+	}
+}
+
 func (p *promise) kill() {
+	atomic.StoreInt32(p.killed, 1)
 	if p.cmd.Process != nil {
-		w, ok := p.cmd.Stdout.(io.ReadCloser)
-		if ok {
-			w.Close()
-		}
-
-		w, ok = p.cmd.Stdin.(io.ReadCloser)
-		if ok {
-			w.Close()
-		}
-
 		p.cmd.Process.Signal(syscall.SIGTERM)
 
 		for ; p.cmd.ProcessState != nil && !p.cmd.ProcessState.Exited(); <-time.After(100 * time.Millisecond) {
