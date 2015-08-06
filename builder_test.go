73b63e14baa017a69cd9042f76aaa8de40b99f4b
Steven Berlanga <zabawaba99@gmail.com>
2015-08-05 20:13:26 -0400

    adding in a test for builder.Close()

--- builder_test.go
+++ builder_test.go
@@ -1,13 +1,24 @@
 package main
 
 import (
-	"github.com/stretchr/testify/assert"
-
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestNewBuilder(t *testing.T) {
 	b, err := NewBuilder(config{})
 	assert.NoError(t, err)
 	assert.NotNil(t, b)
+}
+
+func TestClose(t *testing.T) {
+	b, err := NewBuilder(config{})
+	require.NoError(t, err)
+
+	b.Close()
+
+	_, ok := <-b.done
+	assert.False(t, ok, "channel 'done' was not closed")
 }
